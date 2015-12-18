4fe6663cc36e4e2362ecb707f46089ef3896f18f
Artem Alexandrov <patjoma@gmail.com>
2015-12-18 14:11:44 +0400

    Task 3 - Classes and Objects

--- Preview Lesson/Lession2/Lession2/Program.cs
+++ Preview Lesson/Lession2/Lession2/Program.cs
@@ -1,89 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-// Работа с циклами
+﻿// -----------------------------------------------------------------------
+// <copyright file="GetProviderPopupSearchListCommandProcessor.cs" company="One Call Care Management, Inc.">
+// Copyright (c) One Call Care Management, Inc. All rights reserved.
+// </copyright>
+// <remarks>
+// Работа с циклами ...
+// </remarks>
+// ------
 
 namespace Lession2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //------------------------------------------------------------------------------------------------------------------
-            Console.WriteLine("Lession 2 :  Task 1.");
+	#region Usings
+	using System;
+	#endregion
 
-            //  написать алгоритм решения циклической задачи вычисления значения функции в зависимости от значения переменной 
-            //  аргумента. значения переменной аргумента должны изменяться от начального до конечного с заданным шагом.
-            // Выражение: Y = 2.23X^2 + 3.12X - 1.95
-            // Начальное значение: -0.25
-            // Конечное значение: 1.75
-            // Значение шага: 0.05
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//------------------------------------------------------------------------------------------------------------------
+			Console.WriteLine("Lession 2 :  Task 1.");
 
-
-            //Определяем входные параметры
-            Console.WriteLine("Enter the first value");
-            double A = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the final value");
-            double B = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the step value");
-            double h = Convert.ToDouble(Console.ReadLine());
-
-            //Вычисление значения функции
-
-            Console.WriteLine("Result");
-            for (double X = A; X <= B; X = X + h)
-            {
-                double Y = 2.23 * X * X + 3.12 * X - 1.95;
-                Console.Write(Y + " ");
-            }
-
-            Console.ReadLine();
-
-            //--------------------------------------------------------------------------------------------------------------------
-            //Дано целое N>0. Найти сумму 1+1/2+1/3+...+1/N
-            Console.WriteLine("Lession 2 :  Task 2.");
-
-            Console.WriteLine("Enter N (must be int and  > 0)");
-            int N = Convert.ToInt16(Console.ReadLine());
-            double RezTask2 = 0;
-            int Schetchik = 1;
-            while (Schetchik != N)
-            {
-                RezTask2 = RezTask2 + 1 / Schetchik;
-                Schetchik = Schetchik + 1;
-            }
-            Console.WriteLine("The rezult of calculattion : " + RezTask2);
-            Console.ReadLine();
+			//  написать алгоритм решения циклической задачи вычисления значения функции в зависимости от значения переменной 
+			//  аргумента. значения переменной аргумента должны изменяться от начального до конечного с заданным шагом.
+			// Выражение: Y = 2.23X^2 + 3.12X - 1.95
+			// Начальное значение: -0.25
+			// Конечное значение: 1.75
+			// Значение шага: 0.05
 
 
-            //--------------------------------------------------------------------------------------------------------------------
-            Console.WriteLine("Lession 2 :  Task 3.");
-            //Начальный вклад в банке равен 1000000 руб. Через каждый месяц размер вклада увеличивается на Р процентов 
-            //от имеющейся суммы. Р - вещественное число 0<P<25. По данному Р определить, через сколько месяцев размер 
-            //вклада превысит 1100000 руб. и вывести найденное количество месяцев K (целое число) и итоговый размер 
-            //вклада S (вещественное число).
+			//Определяем входные параметры
+			Console.WriteLine("Enter the first value");
+			double A = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Enter the final value");
+			double B = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Enter the step value");
+			double h = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Сумма вклада 1000000 руб");
-            decimal SumVklada = 1000000;   //S
-            Console.WriteLine("Введите процентную ставку (от 0% до 25%)");
-            int ProcVklada = Convert.ToInt16(Console.ReadLine());
-            
-            int KolMesVklada = 0; //K
+			//Вычисление значения функции
 
-            do
-            {
-                KolMesVklada = KolMesVklada + 1;
-                SumVklada = SumVklada + SumVklada / 100 * ProcVklada;
-            }
-            while (SumVklada <= 1100000);
+			Console.WriteLine("Result");
+			for (double X = A; X <= B; X = X + h)
+			{
+				double Y = 2.23 * X * X + 3.12 * X - 1.95;
+				Console.Write(Y + " ");
+			}
 
-            Console.WriteLine(" Через " + KolMesVklada + " мес. сумма вклада превысит 1100000 и станет равной : " + SumVklada);
-            Console.ReadLine();
+			Console.ReadLine();
+
+			//--------------------------------------------------------------------------------------------------------------------
+			//Дано целое N>0. Найти сумму 1+1/2+1/3+...+1/N
+			Console.WriteLine("Lession 2 :  Task 2.");
+
+			Console.WriteLine("Enter N (must be int and  > 0)");
+			int N = Convert.ToInt16(Console.ReadLine());
+			double RezTask2 = 0;
+			int Schetchik = 1;
+			while (Schetchik != N)
+			{
+				RezTask2 = RezTask2 + 1 / Schetchik;
+				Schetchik = Schetchik + 1;
+			}
+			Console.WriteLine("The rezult of calculattion : " + RezTask2);
+			Console.ReadLine();
 
 
-        }
-    }
+			//--------------------------------------------------------------------------------------------------------------------
+			Console.WriteLine("Lession 2 :  Task 3.");
+			//Начальный вклад в банке равен 1000000 руб. Через каждый месяц размер вклада увеличивается на Р процентов 
+			//от имеющейся суммы. Р - вещественное число 0<P<25. По данному Р определить, через сколько месяцев размер 
+			//вклада превысит 1100000 руб. и вывести найденное количество месяцев K (целое число) и итоговый размер 
+			//вклада S (вещественное число).
+
+			Console.WriteLine("Сумма вклада 1000000 руб");
+			decimal SumVklada = 1000000;   //S
+			Console.WriteLine("Введите процентную ставку (от 0% до 25%)");
+			int ProcVklada = Convert.ToInt16(Console.ReadLine());
+
+			int KolMesVklada = 0; //K
+
+			do
+			{
+				KolMesVklada = KolMesVklada + 1;
+				SumVklada = SumVklada + SumVklada / 100 * ProcVklada;
+			}
+			while (SumVklada <= 1100000);
+
+			Console.WriteLine(" Через " + KolMesVklada + " мес. сумма вклада превысит 1100000 и станет равной : " + SumVklada);
+			Console.ReadLine();
+
+
+		}
+	}
 }
---
+++ Preview Lesson/Lession3/Lession3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lession3
+{
+	public abstract class Shape2DHelper
+	{
+		public abstract double GetSquare();
+		public abstract double GetPerimeter();
+	}
+	public class Rectangle:Shape2DHelper
+	{
+		double a, b;
+		public Rectangle (double a, double b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+		public override double GetSquare()
+		{
+			return a*b;
+		}
+		public override double GetPerimeter()
+		{
+			return (a+b)*2;
+		}
+	}
+	public class Square : Rectangle
+	{
+		double sideSquare;
+		public Square(double sideSquare):base(sideSquare,sideSquare)
+		{
+			this.sideSquare = sideSquare;
+		}
+		public override double GetSquare()
+		{
+			return sideSquare*sideSquare;
+		}
+	}
+	public class Triangle : Shape2DHelper
+	{
+		double a, b, c;
+		public Triangle(double a, double b, double c)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+		public override double GetSquare()
+		{
+			double p = (a + b + c) / 2;
+			return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+		}
+		public override double GetPerimeter()
+		{
+			return a+b+c;
+		}
+	}
+
+	public class TestMyProgram
+	{
+		public static void Main()
+		{
+			Shape2DHelper shapeRectangle = new Rectangle(2, 4);
+			double square = shapeRectangle.GetSquare();
+			double perimeter = shapeRectangle.GetPerimeter();
+			Console.WriteLine("Rectangle: Square - " + square+" ;    perimeter - "+perimeter);
+
+			Shape2DHelper shapeSquare = new Square(8);
+			square = shapeSquare.GetSquare();
+			perimeter = shapeSquare.GetPerimeter();
+			Console.WriteLine("Square: Square - " + square + " ;    perimeter - " + perimeter);
+			
+			Shape2DHelper shapeTriangle = new Triangle(4, 5, 6);
+			square = shapeTriangle.GetSquare();
+			perimeter = shapeTriangle.GetPerimeter();
+			Console.WriteLine("Triangle: Square - " + square + " ;    perimeter - " + perimeter);
+			Console.ReadLine();
+		}
+	}
+}
